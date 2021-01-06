b7707f52a40f0718205f21bb1c4f5b9890886f79
lrdplopes <lrdp.lopes@gmail.com>
2021-01-06 16:00:05 -0300

    new features

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./Home";
 
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -9,6 +9,7 @@ const BlogList = ({ blogs, title }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button>{handleDelete}</button>
         </div>
       ))}
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -7,6 +7,15 @@ const Home = () => {
     { title: "Welcome party!", body: "loren ipsum", author: "yoshi", id: 2 },
     { title: "Web dev top tips", body: "loren ipsum", author: "luigi", id: 3 },
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log(blogs);
+  }, []);
 
   return (
     <div className="home">
